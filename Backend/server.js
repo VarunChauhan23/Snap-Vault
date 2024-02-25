47fd24c2a8bf4cb144547a1e661397ba0663b1dd
Varun Chauhan <agrasector@gmail.com>
2024-02-25 16:38:42 +0530

    minor changes

--- Backend/server.js
+++ Backend/server.js
@@ -1,24 +1,24 @@
-const express = require('express');
-require("dotenv").config({ path: './config.env' });
-const connectToMongo = require('./db');
+const express = require("express");
+require("dotenv").config({ path: "./config.env" });
+const connectToMongo = require("./db");
 const app = express();
-const port = process.env.app_Port
-// const cors = require('cors')
+const port = process.env.app_Port;
+const cors = require("cors");
 
-connectToMongo().catch(console.dir);;
+connectToMongo().catch(console.dir);
 
-// const corsOptions = {
-//   origin: 'https://snap-vault.vercel.app'
-// };
-
-// app.use(cors(corsOptions));
+app.use(
+  cors({
+    origin: "https://snap-vault.vercel.app",
+  })
+);
 
 app.use(express.json());
 
 // Available routes are -->
-app.use('/api/auth', require('./routes/auth'));
-app.use('/api/image', require('./routes/images'));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/image", require("./routes/images"));
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
